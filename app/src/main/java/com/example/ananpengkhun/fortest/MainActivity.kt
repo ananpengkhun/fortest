a96f49e7727553d4ccc6b6274f94b879875cbc5a
rafa <se56160422@gmail.com>
2019-02-18 17:13:38 +0700

    ssss

--- app/src/main/java/com/example/ananpengkhun/fortest/MainActivity.kt
+++ app/src/main/java/com/example/ananpengkhun/fortest/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.ananpengkhun.fortest
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(animation_view.isAnimating){
+            println("animation_view running")
+
+        }
     }
 }
